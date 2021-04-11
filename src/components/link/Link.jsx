be0127f0973ec78deadfc68928efa455d6eadba7
Yaroslav Losev <losevyaroslav@gmail.com>
2021-04-11 22:55:14 +0300

    Disable eslint jsx-a11y/anchor-is-valid warning on link

--- src/components/link/Link.jsx
+++ src/components/link/Link.jsx
@@ -1,6 +1,7 @@
 import "./styles.scss";
 
 const Link = ({ className, children }) => (
+  // eslint-disable-next-line jsx-a11y/anchor-is-valid
   <a className={`link ${className}`} href="#">
     {children}
   </a>
